94d8273fe19fd66d459f1bf99d617a9ab7dd477b
Thuantran2910 <99887435+Thuantran2910@users.noreply.github.com>
2022-05-06 09:43:53 +0700

    Quản_lí_Nhân_viên

--- Program.cs
+++ Program.cs
@@ -1,124 +1,116 @@
-﻿using System;
+﻿/*
+Công ty XYZ muốn viết phần mềm quản lý nhân viên, mô tả hệ thống như sau:
+-   Mỗi nhân viên sẽ thuộc một phòng ban, thông tin của phòng ban
+(mã phòng ban, tên phòng ban, trưởng phòng hiện tại), thông tin
+của mỗi nhân viên (Mã nhân viên, tên nhân viên, ngày sinh, chức vụ). 
+Theo chính sách công ty có các chức vụ: Giám đốc( quản lý chung), trưởng phòng,
+nhân viên. Lương cơ bản của toàn bộ nhân viên trong công ty là 10 triệu/tháng.
+Giám đốc phụ cấp chức vụ 25%, trưởng phòng phụ cấp chức vụ 15%, phó phòng phụ cấp 
+5% nhân viên ko có phụ cấp chức vụ.
+-   Chương trình cung cấp các chức năng:
+    +   Thêm,xuat, sửa, xóa, sắp xếp, tìm Phòng ban, nhân viên.
+    +   Thống kê tổng số lương phải trả cho nhân viên trong 1 tháng.
+
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace tra_cuu_tu_dien_Anh_Viet
+namespace QuanLyNhanVien
 {
     class Program
     {
-        static Dictionary<string, string> dic = new Dictionary<string, string>();//tra cứu từ nhập tiếng anh ra tiếng việt;
+        static List<PhongBan> dsPB = new List<PhongBan>();
+        static void TessQLNV()
+        {
+                
+            PhongBan pNS = new PhongBan();
+            pNS.MaPB = 1;
+            pNS.TPB = "phong nhan su";
+            dsPB.Add(pNS);
+
+            NhanVien teo = new NhanVien();
+            teo.MaNV = 1;
+            teo.TNV = "uyen";
+            teo.ChucVu = LoaiChucVu.Truong_phong;
+            pNS.ThemNhanVien(teo);
+
+            NhanVien ty = new NhanVien();
+            ty.MaNV = 2;
+            ty.TNV = "uyen";
+            ty.ChucVu = LoaiChucVu.Nhan_Vien;
+            pNS.ThemNhanVien(ty);
+
+            NhanVien met = new NhanVien();
+            met.MaNV = 45;
+            met.TNV = "Teo";
+            met.ChucVu = LoaiChucVu.Nhan_Vien;
+            pNS.ThemNhanVien(met);
+
+
+            PhongBan pkt = new PhongBan();
+            pkt.MaPB = 2;
+            pkt.TPB = "PHONG KE TOAN";
+            dsPB.Add(pkt);
+
+            NhanVien bin = new NhanVien();
+            bin.MaNV = 3;
+            bin.TNV = "Bin bin bin";
+            bin.ChucVu = LoaiChucVu.Pho_Phong;
+            pkt.ThemNhanVien(bin);
+
+
+            Console.WriteLine("Danh sach toan bo nhan vien trong cong ty:");
+            foreach(PhongBan pb in dsPB)
+            {
+                Console.WriteLine(pb.TPB);
+                pb.XuatToanBoNhanVien();
+            }
+            NhanVien old = pkt.TimNV(3);
+            if (old != null)
+            {
+                old.TNV = "Bim bim bim";
+            }
+            Console.WriteLine("Ds toan bo nhan vien tong cong ty sau khi sua:");
+            foreach (PhongBan pb in dsPB)
+            {
+                Console.WriteLine(pb.TPB);
+                pb.XuatToanBoNhanVien();
+            }
+            if (pNS.XoaNV(113) == false)
+            {
+                Console.WriteLine("Ko tim thay ma nv 113 de xoa");
+            }
+            else
+            {
+                Console.WriteLine("Ds toan bo nhan vien tong cong ty sau khi xoa:");
+                foreach (PhongBan pb in dsPB)
+                {
+                    Console.WriteLine(pb.TPB);
+                    pb.XuatToanBoNhanVien();
+                }
+            }
+            Console.WriteLine("Danh sach toan bo nhan vien trong cong ty:");
+            pNS.XuatToanBoNhanVien();
+            
+            pNS.Sapxep();
+            Console.WriteLine("Danh sach toan bo nhan vien trong cong ty sau khi sap xep:");
+
+            pNS.XuatToanBoNhanVien();
+
+            long sum = 0;
+            foreach(PhongBan pb in dsPB) 
+                sum+= pb.TongLuong();
+            Console.WriteLine("tong luong can thanh toan la: {0}",sum);
+
+        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            while (true)
-            {
-                menu();
-                Console.WriteLine("Ban co tiep tuc su dung tu dien ko?(c/k):");
-                string s = Console.ReadLine();
-                if (s == "k")
-                    break;
-            }
-            Console.WriteLine("Bye bye!");
-        }
-
-        private static void menu()
-        {
-            Console.WriteLine("1.Tạo từ mới");
-            Console.WriteLine("2.Sửa từ");
-            Console.WriteLine("3.tra cứu từ");
-            Console.WriteLine("4.xóa từ");
-            Console.WriteLine("Bạn chọn chức năng nào?");
-            try
-            {
-                int cn = int.Parse(Console.ReadLine());
-                switch (cn)
-                {
-                    case 1:
-                        Taotumoi();
-                        break;
-                    case 2:
-                        Suatu();
-                        break;
-                    case 3:
-                        Tracuu();
-                        break;
-                    case 4:
-                        Xoatu();
-                        break;
-                    default:
-                        Console.WriteLine("Bạn chọn nhậm chức năng rồi!");
-                        break;
-                }
-            }catch (Exception ex)
-            {
-                Console.WriteLine("lỗi gì đó: " + ex.Message);
-            
-        }
-    }
-
-        private static void Xoatu()
-        {
-            Console.WriteLine("nhập vào từ muốn xóa:");
-            string ta = Console.ReadLine();
-            if (dic.ContainsKey(ta))
-            {
-                dic.Remove(ta);
-                Console.WriteLine("xóa thành công từ [(0)]",ta);
-            }
-            else
-            {
-                Console.WriteLine("ko tìm thấy từ [{0}] để xóa", ta);
-            }
-        }
-
-        private static void Tracuu()
-        {
-            Console.WriteLine("nhập vào từ tiếng anh:");
-            string ta = Console.ReadLine();
-            if (dic.ContainsKey(ta))
-            {
-                string tv = dic[ta];
-                Console.WriteLine("nghĩa của [{0}] là [{1}]",ta,tv);
-            }
-            else
-            {
-                Console.WriteLine("từ điển chưa cập nhập từ [{0}]", ta);
-            }
-
-        }
-
-        private static void Suatu()
-        {
-            Console.WriteLine("nhập vào tiếng anh để sửa lại nghĩa:");
-            string ta = Console.ReadLine();
-            if (dic.ContainsKey(ta))
-            {
-                Console.WriteLine("Ko tìm thấy {0} để sửa", ta);
-            }
-            else
-            {
-                Console.WriteLine("mời bạn nhập lại nghĩa tiếng việt");
-                string tv = Console.ReadLine();
-                dic[ta] = tv;
-            }
-        }
-
-        private static void Taotumoi()
-        {
-            Console.WriteLine("nhập từ tiếng anh");
-            string ta = Console.ReadLine();
-            if (dic.ContainsKey(ta))
-            {
-                Console.WriteLine("từ {0} đã tồn tại rồi", ta);
-            }
-            else
-            {
-                Console.WriteLine("mời nhập nghĩa tiếng việt:");
-                string tv = Console.ReadLine();
-                dic.Add(ta, tv);
-            }  
+            TessQLNV();
+            Console.ReadLine();
         }
     }
 }
